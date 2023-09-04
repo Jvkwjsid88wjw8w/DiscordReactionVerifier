4ff9b04ceae06eccf22b48340257f7783446d0e9
Jvkwjsid88wjw8w <143795696+Jvkwjsid88wjw8w@users.noreply.github.com>
2023-09-04 16:14:41 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    role_name = ""
-    message_id = ""
+    role_name = "Sounder store"
+    message_id = "1147883755361538122"
     prefix = "."
-    bot_token = ""
-    channel_id = ""
+    bot_token = "MTE0ODMzNTA5NzU5MDg1Nzg2OQ.GJGash.Atj_9Ih4H-MAJdRlfd7aTZiVu922_5ICZo2aHQ"
+    channel_id = "1144476443150127206"
